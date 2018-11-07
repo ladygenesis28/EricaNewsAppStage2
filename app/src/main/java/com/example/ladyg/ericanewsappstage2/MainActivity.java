2c188617d3f6e96014e14f7637eb8467e4160c48
ladygenesis28 <ladygenesis28@yahoo.com>
2018-11-06 21:20:17 -0500

    for udacity part 6 project

--- app/src/main/java/com/example/ladyg/ericanewsappstage2/MainActivity.java
+++ app/src/main/java/com/example/ladyg/ericanewsappstage2/MainActivity.java
@@ -16,6 +16,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -25,10 +26,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     private static final String JSON_URL_NEWS = "http://content.guardianapis.com/search?format=json&show-tags=contributor&api-key=b3ee86e0-e862-44ff-81e5-421bd5983e37";
 
-    /**
-     * Tag for the log messages
-     */
-    public static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static final String LOG_TAG = MainActivity.class.getName();
 
     private static final int NEWS_LOADER_ID = 1;
 
@@ -67,7 +65,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         // So we know when the user has adjusted the query settings
         prefs.registerOnSharedPreferenceChangeListener(this);
 
-
         // Get a reference to the LoaderManager, in order to interact with loaders.
         LoaderManager getLoaderManager = getLoaderManager();
 
@@ -93,89 +90,89 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             mEmptyStateTextView.setText(R.string.no_internet_connection);
         }
         @Override
-        public void onSharedPreferenceChanged (SharedPreferences prefs, String s){
-            if (key.equals(getString(R.string.settings_min_magnitude_key)) ||
-                    key.equals(getString(R.string.settings_order_by_key))) {
-                // Clear the ListView as a new query will be kicked off
-                mAdapter.clear();
+        public void onSharedPreferenceChanged(SharedPreferences prefs, String s){
+                if (key.equals(getString(R.string.settings_min_magnitude_key)) ||
+                        key.equals(getString(R.string.settings_order_by_key))) {
+                    // Clear the ListView as a new query will be kicked off
+                    mAdapter.clear();
 
-                // Hide the empty state text view as the loading indicator will be displayed
-                mEmptyStateTextView.setVisibility(View.GONE);
+                    // Hide the empty state text view as the loading indicator will be displayed
+                    mEmptyStateTextView.setVisibility(View.GONE);
 
-                // Show the loading indicator while new data is being fetched
-                View loadingIndicator = findViewById(R.id.loading_indicator);
-                loadingIndicator.setVisibility(View.VISIBLE);
+                    // Show the loading indicator while new data is being fetched
+                    View loadingIndicator = findViewById(R.id.loading_indicator);
+                    loadingIndicator.setVisibility(View.VISIBLE);
 
-                // Restart the loader to requery the USGS as the query settings have been updated
-                getLoaderManager().restartLoader(NEWS_LOADER_ID, null, this);
-            }
-
-            @Override
-            public Loader<List<News>> onCreateLoader ( int i, Bundle bundle){
-                SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-                String minNews = sharedPrefs.getString(
-                        getString(R.string.settings_min_news_key),
-                        getString(R.string.settings_min_news_default));
-
-                String orderBy = sharedPrefs.getString(
-                        getString(R.string.settings_order_by_key),
-                        getString(R.string.settings_order_by_default)
-                );
-
-                Uri baseUri = Uri.parse(JSON_URL_NEWS);
-                Uri.Builder uriBuilder = baseUri.buildUpon();
-
-                uriBuilder.appendQueryParameter("format", "geojson");
-                uriBuilder.appendQueryParameter("limit", "10");
-                uriBuilder.appendQueryParameter("minnews", minNews);
-                uriBuilder.appendQueryParameter("orderby", "dates");
-
-                return new NewsLoader(this, uriBuilder.toString());
-            }
-
-            @Override
-            public void onLoadFinished (Loader < List < News >> loader, List < News > data){
-
-                // Otherwise, display error
-                // First, hide loading indicator so error message will be visible
-                View loadingIndicator = findViewById(R.id.loading_indicator);
-                loadingIndicator.setVisibility(View.GONE);
-
-                // Set empty state text to display "No com.example.ladyg.ericanewsappstage2.News found."
-                mEmptyStateTextView.setText(R.string.no_news_found);
-
-                mAdapter.clear();
-
-                mAdapter = new RecyclerViewAdapter(MainActivity.this, data);
-                if (data != null && !data.isEmpty()) {
-                    recyclerView.setAdapter(mAdapter);
-
+                    // Restart the loader to requery the USGS as the query settings have been updated
+                    getLoaderManager().restartLoader(NEWS_LOADER_ID, null, this);
                 }
-            }
 
-            @Override
-            public void onLoaderReset (Loader < List < News >> loader) {
-                // Loader reset, so we can clear out our existing data.
-                mAdapter.clear();
-            }
+                @Override
+                public Loader<List<News>> onCreateLoader ( int i, Bundle bundle){
+                    SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+                    String minNews = sharedPrefs.getString(
+                            getString(R.string.settings_min_news_key),
+                            getString(R.string.settings_min_news_default));
 
-            @Override
-            public boolean onCreateOptionsMenu (Menu menu){
-                getMenuInflater().inflate(R.menu.main, menu);
-                return true;
-            }
+                    String orderBy = sharedPrefs.getString(
+                            getString(R.string.settings_order_by_key),
+                            getString(R.string.settings_order_by_default)
+                    );
 
-            @Override
-            public boolean onOptionsItemSelected (MenuItem item){
-                int id = item.getItemId();
-                if (id == R.id.action_settings) {
-                    Intent settingsIntent = new Intent(this, SettingsActivity.class);
-                    startActivity(settingsIntent);
+                    Uri baseUri = Uri.parse(JSON_URL_NEWS);
+                    Uri.Builder uriBuilder = baseUri.buildUpon();
+
+                    uriBuilder.appendQueryParameter("format", "geojson");
+                    uriBuilder.appendQueryParameter("limit", "10");
+                    uriBuilder.appendQueryParameter("minnews", minNews);
+                    uriBuilder.appendQueryParameter("orderby", "dates");
+
+                    return new NewsLoader(this, uriBuilder.toString());
+                }
+
+                @Override
+                public void onLoadFinished (Loader < List < News >> loader, List < News > data){
+
+                    // Otherwise, display error
+                    // First, hide loading indicator so error message will be visible
+                    View loadingIndicator = findViewById(R.id.loading_indicator);
+                    loadingIndicator.setVisibility(View.GONE);
+
+                    // Set empty state text to display "No com.example.ladyg.ericanewsappstage2.News found."
+                    mEmptyStateTextView.setText(R.string.no_news_found);
+
+                    mAdapter.clear();
+
+                    mAdapter = new RecyclerViewAdapter(MainActivity.this, data);
+                    if (data != null && !data.isEmpty()) {
+                        recyclerView.setAdapter(mAdapter);
+
+                    }
+                }
+
+                @Override
+                public void onLoaderReset (Loader < List < News >> loader) {
+                    // Loader reset, so we can clear out our existing data.
+                    mAdapter.clear();
+                }
+
+                @Override
+                public boolean onCreateOptionsMenu (Menu menu){
+                    getMenuInflater().inflate(R.menu.main, menu);
                     return true;
                 }
-                return super.onOptionsItemSelected(item);
+
+                @Override
+                public boolean onOptionsItemSelected (MenuItem item){
+                    int id = item.getItemId();
+                    if (id == R.id.action_settings) {
+                        Intent settingsIntent = new Intent(this, SettingsActivity.class);
+                        startActivity(settingsIntent);
+                        return true;
+                    }
+                    return super.onOptionsItemSelected(item);
+                }
             }
         }
     }
 }
-
